3b06d15944b00f3a8aad484f631f25e00084e654
esraa <jbesraa@gmail.com>
2024-02-11 14:19:34 +0200

    cleanup home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,55 +2,40 @@ import "../App.css";
 import { useNodeContext } from "../NodeContext";
 import { useEffect, useState } from "react";
 import { useRouterContext } from "../router";
+import BoltIcon from "@mui/icons-material/Bolt";
 
 function Home() {
-	const {
-		create_wallet,
-		start_node,
-		get_node_id,
-		is_node_running,
-		get_esplora_address,
-		get_listening_address,
-		get_onchain_balance,
-		get_onchain_address,
-		send_onchain_transaction,
-	} = useNodeContext();
+	const { start_node, is_node_running, get_onchain_balance } =
+		useNodeContext();
 	const { push_route } = useRouterContext();
 	const [balance, setBalance] = useState("0");
-	const [nodeId, setNodeId] = useState("");
-	const [address, setAddress] = useState("");
-	const [listeningAddress, setListeningAddress] = useState("");
-	const [esploraAddress, setEsploraAddress] = useState("");
 	const [isRunning, setIsRunning] = useState(false);
-	// useEffect(() => {
-	// 	create_wallet({} as CreateWalletInput);
-	// 	// list_ws();
-	// 	// createdirs()
-	// }, []);
+
+	useEffect(() => {
+		const handler = async () => {
+			const isRunning = await is_node_running();
+			if (!isRunning) {
+				await start_node();
+			}
+		};
+		handler();
+	}, []);
 
 	useEffect(() => {
 		const timer = setInterval(async () => {
-			const isRunning = await is_node_running();
-			const onchain_balance = await get_onchain_balance();
-			setBalance(onchain_balance);
-			setIsRunning(isRunning);
-		}, 5000);
+			if (!isRunning) {
+				const isRunning = await is_node_running();
+				setIsRunning(isRunning);
+			}
+		}, 10);
 		return () => clearInterval(timer);
 	}, []);
 
 	useEffect(() => {
 		const handler = async () => {
 			if (isRunning) {
-				const id = await get_node_id();
-				const esplora = await get_esplora_address();
-				const listening = await get_listening_address();
-				// const onchain_balance = await get_onchain_balance();
-				const onchain_address = await get_onchain_address();
-				// setBalance(onchain_balance);
-				setAddress(onchain_address);
-				setNodeId(id);
-				setEsploraAddress(esplora);
-				setListeningAddress(listening);
+				const onchain_balance = await get_onchain_balance();
+				setBalance(onchain_balance);
 			}
 		};
 		handler();
@@ -58,29 +43,12 @@ function Home() {
 
 	return (
 		<div className="container">
-			<h1>{balance} SATS</h1>
-			<div style={{ padding: "1em" }}>
-				<button
-					disabled={true}
-					onClick={() => create_wallet()}
-					style={{ width: "100%" }}
-				>
-					create wallet
-				</button>
-			</div>
-			<div style={{ padding: "1em" }}>
-				<button
-					disabled={isRunning}
-					onClick={() => start_node()}
-					style={{ width: "100%" }}
-				>
-					start node
-				</button>
-			</div>
-			<div>{nodeId}</div>
-			<div>{String(isRunning)}</div>
-			<div>esplora address: {esploraAddress}</div>
-			<div>listening address: {listeningAddress}</div>
+			<h1>
+				{balance} SATS
+				<span>
+					<BoltIcon color={isRunning ? "success" : "error"} />
+				</span>
+			</h1>
 			<div style={{ padding: "1em" }}>
 				<button
 					onClick={() => push_route("send")}
